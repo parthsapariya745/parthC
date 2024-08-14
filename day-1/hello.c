fe5e271f57605f56063b60f8e900e114cec8bc57
parthsapariya745 <parthsapariyait7@gmail.com>
2024-08-14 15:04:08 +0530

    day1

---
+++ day-1/hello.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int x,y,Ans;
+    printf("x = ");
+    scanf("%d",x);
+
+    printf("y = ");
+    scanf("%d",y);
+
+    Ans = (x + y)*(x + y);
+    printf("Ans = %d",Ans);
+}
