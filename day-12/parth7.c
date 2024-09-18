a50062f04ed7a552616103229c2b96e9894d46d3
parthsapariya745 <parthsapariyait7@gmail.com>
2024-09-18 11:23:36 +0530

    parthpattern

--- day-12/parth7.c
+++ day-12/parth7.c
@@ -1,21 +1,21 @@
-#include<stdio.h>
+int main() {
+    int raw, col;
 
- int main(){
-    int raw,col;
-    for (raw = 1; raw <= 5; raw++)
+    for(raw=1;raw<=5;raw++)
     {
-        for (col = 1; col <= 5; col++)
+        for(col=1 ; col<=5;col++){
+            if(raw==2 && (col>1 && col<5) || raw==4 && col>1 || raw==5 && col>1 )
         {
-            printf("* ");
+            printf("  ");
         }
-        // if (raw > 1 && raw < 5 || col <= 1 && raw > 4)
-        // {
-        //     printf("    ");
-        // }
-        // else
-        // {
-        //     printf("* ");
-        // }
+        else 
+        {
+            printf(" *");
+        }
+        }
+
         printf("\n");
+       
     }
+
 }
