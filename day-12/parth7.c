27ccbd0271b76c988d37730d58798e422e942711
parthsapariya745 <parthsapariyait7@gmail.com>
2024-12-16 22:45:38 +0530

    error2

--- day-12/parth7.c
+++ day-12/parth7.c
@@ -1,18 +1,19 @@
+#include <stdio.h>
+
 int main() {
     int raw, col;
 
-    for(raw = 1; raw <= 5; raw++)
-    {
-        for(col = 1; col <= 5; col++){
-            if(raw == 2 && (col > 1 && col < 5) || raw == 4 && col > 1 || raw == 5 && col > 1)
-            {
-                printf("  ");
-            }
-            else 
-            {
-                printf(" *");
+    for (raw = 1; raw <= 5; raw++) {
+        for (col = 1; col <= 5; col++) {
+            if ((raw == 2 && col > 1 && col < 5) || 
+                (raw == 4 && col > 1) || 
+                (raw == 5 && col > 1)) {
+                printf("  "); 
+            } else {
+                printf(" *"); 
             }
         }
         printf("\n");
     }
+    return 0;
 }
